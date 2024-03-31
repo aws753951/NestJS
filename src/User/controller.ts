4592263fc110b0912647cb99e98358a4f47b50c5
Marc Z <a0983204766@gmail.com>
2024-03-31 18:31:50 +0800

    引用Logger + 自定義Logger的名稱

--- src/User/controller.ts
+++ src/User/controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { UserService } from './service';
 import { CoreService } from 'src/Core/service';
 
 @Controller('user')
 export class userController {
+  private readonly logger = new Logger(userController.name); // 自定義logger描述
   constructor(
     private readonly userService: UserService,
     private readonly coreService: CoreService,
@@ -16,6 +17,9 @@ export class userController {
 
   @Get('core')
   getCore(): string {
+    console.log('console.log');
+    Logger.log('Nest Logger'); // 使用Logger，在使用需變成 Logger.log
+    this.logger.log('this.logger'); // 使用自己定義的log : this.logger，但背後是 Logger這個，所以需要 this.logger.log
     return this.coreService.getCore();
   }
 }
